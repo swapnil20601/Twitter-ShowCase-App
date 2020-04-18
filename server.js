f9ccb12c827e62fcf73a6ef6b1267319a765298f
swapnil <swap47233@gmail.com>
2020-04-18 12:38:28 -0500

    Added server.js file to create node server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Bodyparse middleware
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Sever stared on ${port}`))
