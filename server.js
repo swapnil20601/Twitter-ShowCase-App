a40f6247f92f8801f138ba018e37ae22e4dc547f
Swapnil Jadhav <swapniljadhav@Swapnils-MacBook-Pro.local>
2020-04-30 17:29:16 -0500

    Building basic server side code to grab recent tweets

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
+
 
 //Bodyparse middleware
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000
+const url = "https://api.twitter.com/1.1/search/tweets.json";
 
-app.listen(port, () => console.log(`Server stared on ${port}`))
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAALf9DgEAAAAA9TgreZfbY0DuKStBv3tOFzqEXmg%3D5KKGnpIKtZfHUvsKMwd2Kn0PAK7bSMyd34u5b5ascyfN2z7Dmr",
+  Accept: "application/json"
+};
+
+const params = {
+  q: 'nasa',
+  result_type: 'recent',
+  count: 1
+}
+
+axios.get(url, {params, headers}).then(res => {
+  console.log(res.data)
+}).catch(error => {
+  console.log('Error is ', error)
+})
+
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server stared on ${port}`));
