3aefced7604b62d82f6599e7607f1d3a5df4ec70
Swapnil Jadhav <swapniljadhav@Swapnils-MacBook-Pro.local>
2020-04-30 22:10:33 -0500

    Added random search tweet server code

--- server.js
+++ server.js
@@ -1,33 +1,75 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require('axios');
-const cors = require('cors');
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
-
 
 //Bodyparse middleware
 app.use(bodyParser.json());
 
 const url = "https://api.twitter.com/1.1/search/tweets.json";
 
+const randomCount = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
 const headers = {
   "Content-Type": "application/json",
-  Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAALf9DgEAAAAA9TgreZfbY0DuKStBv3tOFzqEXmg%3D5KKGnpIKtZfHUvsKMwd2Kn0PAK7bSMyd34u5b5ascyfN2z7Dmr",
-  Accept: "application/json"
+  Authorization:
+    "Bearer AAAAAAAAAAAAAAAAAAAAALf9DgEAAAAA9TgreZfbY0DuKStBv3tOFzqEXmg%3D5KKGnpIKtZfHUvsKMwd2Kn0PAK7bSMyd34u5b5ascyfN2z7Dmr",
+  Accept: "application/json",
 };
 
-const params = {
-  q: 'nasa',
-  result_type: 'recent',
-  count: 1
-}
+// const params = {
+//   q: "nasa",
+//   result_type: "recent",
+//   count: randomCount(5,10)
+// };
 
-axios.get(url, {params, headers}).then(res => {
-  console.log(res.data)
-}).catch(error => {
-  console.log('Error is ', error)
-})
+const params = {
+  q: "nasa",
+  result_type: "popular",
+  count: 100
+};
+
+//search for 5-10 tweets for a query parameter passed
+// axios
+//   .get(url, { params, headers })
+//   .then((res) => {
+//     for (const tweet of res.data.statuses) {
+//      console.log('CREATD ON',tweet.created_at);
+//      console.log('TWEET ',tweet.text);
+//      console.log('RETWEET COUNT',tweet.retweet_count);
+//      console.log('FAVORITE COUNT',tweet.favorite_count);
+//      console.log('USER NAME',tweet.user.name);
+//      console.log('image',tweet.user.profile_image_url);
+//      console.log();
+//     }
+//   })
+//   .catch((error) => {
+//     console.log("Error is ", error);
+//   });
+
+axios
+  .get(url, { params, headers })
+  .then((res) => {
+    const tweets = res.data.statuses;
+    const randomIndex = (tweets) => {
+      let index = Math.floor(Math.random() * tweets.length);
+      return tweets[index];
+    }
+
+    let randomTweet = randomIndex(tweets)
+     console.log('CREATD ON',randomTweet.created_at);
+     console.log('TWEET ',randomTweet.text);
+     console.log('RETWEET COUNT',randomTweet.retweet_count);
+     console.log('FAVORITE COUNT',randomTweet.favorite_count);
+     console.log('USER NAME',randomTweet.user.name);
+     console.log('image',randomTweet.user.profile_image_url);
+     console.log();
+    })
+  .catch((error) => {
+    console.log("Error is ", error);
+  });
 
 app.use(cors());
 
